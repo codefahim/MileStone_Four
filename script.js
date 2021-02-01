0a7d20e645251698678bdfac50eb411df9dda028
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-02-01 10:36:49 +0600

    js code simplyfy

--- script.js
+++ script.js
@@ -1,104 +1,109 @@
 // JavaScript Code Start From here
+var total;
+var subtotal;
+var vat;
+var economyClassSets;
+var firstClassSets;
 
 // FirstClass Sets coundown Claculation[plus]
 const plusForFirstClass = document.getElementById('plus');
-plusForFirstClass.addEventListener('click',function(){
-productCostHandle('firstClassInput',true);
-totalCostCalculation();
+plusForFirstClass.addEventListener('click', function() {
+    productCostHandle('firstClassInput', true);
+    totalCostCalculation();
 });
 
 // FirstClass Sets coundown Claculation[minus]
 const minusForFirstClass = document.getElementById('minus');
-minusForFirstClass.addEventListener('click',function(){
-productCostHandle('firstClassInput', false);
-totalCostCalculation();
+minusForFirstClass.addEventListener('click', function() {
+    productCostHandle('firstClassInput', false);
+    totalCostCalculation();
 });
 
 // EconomyClass Sets coundown Claculation[plus]
 const plusEconomyClass = document.getElementById('plusOne');
-plusEconomyClass.addEventListener('click',function(){
-productCostHandle('economyClassInput',true);
-totalCostCalculation();
+plusEconomyClass.addEventListener('click', function() {
+    productCostHandle('economyClassInput', true);
+    totalCostCalculation();
 });
 
 
 // EconomyClass Sets coundown Claculation[minus]
 const minusEconomyClass = document.getElementById('minusOne');
-minusEconomyClass.addEventListener('click',function(){
-productCostHandle('economyClassInput',false);
-totalCostCalculation();
+minusEconomyClass.addEventListener('click', function() {
+    productCostHandle('economyClassInput', false);
+    totalCostCalculation();
 });
 
 
 
 // Total Costing for FirstClass and EconomyClass Sets 
 
-function totalCostCalculation(){
-	let firstClassInput = document.getElementById('firstClassInput').value;
-	let firstClassSets = parseInt(firstClassInput);
-	let firstCost = 150* firstClassSets;
+
+function totalCostCalculation() {
+    let firstClassInput = document.getElementById('firstClassInput').value;
+    firstClassSets = parseInt(firstClassInput);
+    let firstCost = 150 * firstClassSets;
 
 
-	let economyClassInput = document.getElementById('economyClassInput').value;
-	let economyClassSets = parseInt(economyClassInput);
-	let economyCost = 100 * economyClassSets;
+    let economyClassInput = document.getElementById('economyClassInput').value;
+    economyClassSets = parseInt(economyClassInput);
+    let economyCost = 100 * economyClassSets;
+    subtotal = document.getElementById('subtotal').innerHTML;
+    subtotal = firstCost + economyCost;
+    document.getElementById('subtotal').innerHTML = subtotal;
+    vat = subtotal * .1;
+    document.getElementById('vat').innerHTML = vat;
+    total = subtotal + vat;
+    document.getElementById('total').innerHTML = total;
 
-	let subtotal = document.getElementById('subtotal').innerHTML ;
-	subtotal = firstCost+economyCost;
-
-document.getElementById('subtotal').innerHTML = subtotal;
- let vat = subtotal*.1;
-document.getElementById('vat').innerHTML = vat;
-let total =subtotal+vat;
-
-document.getElementById('total').innerHTML = total;
-	
 }
 
 // Function for Sets incrise or decrise 
 
-function productCostHandle(id,isIncrease){
-let productQuantity = document.getElementById(id).value;
-let conformedSets = parseInt(productQuantity);
-if (isIncrease == true){
-	conformedSets++;
-}
-if (isIncrease == false){
-	if(conformedSets >=1){
-		conformedSets--;	
-	}
-}
-        
-document.getElementById(id).value=conformedSets;
+function productCostHandle(id, isIncrease) {
+    let productQuantity = document.getElementById(id).value;
+    let conformedSets = parseInt(productQuantity);
+    if (isIncrease == true) {
+        conformedSets++;
+    }
+    if (isIncrease == false) {
+        if (conformedSets >= 1) {
+            conformedSets--;
+        }
+    }
+
+    document.getElementById(id).value = conformedSets;
 };
 
 
 // confirmation Booking
-const total = document.getElementById('total').innerHTML;
+
+var conformText = document.getElementById('successText');
 
 
 const bookingConfirmation = document.getElementById('bookNow');
-bookingConfirmation.addEventListener('click',function(){
-	let mainSection =document.getElementById('mainSection');
-	mainSection.style.display = 'none';
+bookingConfirmation.addEventListener('click', function() {
+    let mainSection = document.getElementById('mainSection');
+    mainSection.style.display = 'none';
 
-	let confirmation =document.getElementById('confirmation');
-	confirmation.style.display = 'block';
+    let confirmation = document.getElementById('confirmation');
+    confirmation.style.display = 'block';
+    if (total >= 10) {
+        document.getElementById('firstClassPassenger').innerHTML = firstClassSets;
+        document.getElementById('economyClassPassenger').innerHTML = economyClassSets;
+        document.getElementById('subCost').innerHTML = subtotal;
+        document.getElementById('vatCost').innerHTML = vat;
+        document.getElementById('totalCost').innerHTML = total;
+        conformText.style.color = 'green';
 
- const firstClassPassenger =document.getElementById('firstClassInput').value;
-document.getElementById('firstClassPassenger').innerHTML=firstClassPassenger;
+    } else {
+        const ticket = "Your tickets are not confirmed,Please Select a sets First.";
+        conformText.innerHTML = ticket;
+        conformText.style.color = 'red';
+        conformText.style.fontSize = '21px';
+    }
 
-const economyClassPassenger =document.getElementById('economyClassInput').value;
-document.getElementById('economyClassPassenger').innerHTML=economyClassPassenger;
 
-	const subCost =document.getElementById('subtotal').innerHTML;
-	document.getElementById('subCost').innerHTML= subCost;
-
-	const vatCost =document.getElementById('vat').innerHTML;
-	document.getElementById('vatCost').innerHTML =vatCost;
-
-	const total = document.getElementById('total').innerHTML;
-	document.getElementById('totalCost').innerHTML=total;
 
 
 });
